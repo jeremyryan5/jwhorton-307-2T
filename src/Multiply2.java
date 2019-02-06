f85c5564b0768355c84a9f7a6642aeaf20159bcd
Jeremy Whorton <jeremywhortonclark@yahoo.com>
2019-02-06 11:32:01 -0800

    Add project again

---
+++ src/Multiply2.java
@@ -0,0 +1,11 @@
+public class Multiply2 {
+	
+	public static int multiply2(int x, int y) {
+		return x*y;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(multiply2(2,600));
+	}
+
+}
